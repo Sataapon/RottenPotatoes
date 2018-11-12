ee93f19384389375ca40d80220d2b57cfb1f40de
EC2 Default User <ec2-user@ip-172-31-11-114.us-east-2.compute.internal>
2018-11-12 07:57:56 +0000

    Add asscociations Movie, Moviesgoer and Review models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+    has_many :reviews
+    
     def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end #  shortcut: array of strings
     validates :title, :presence => true
     validates :release_date, :presence => true
