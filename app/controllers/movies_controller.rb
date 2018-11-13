d2e974c19bcfe9feedb1fb96e2f30ff0a2d1f846
EC2 Default User <ec2-user@ip-172-31-11-114.us-east-2.compute.internal>
2018-11-13 18:00:24 +0000

    Edit search_tmdb action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -46,9 +46,7 @@ class MoviesController < ApplicationController
     end
     
     def search_tmdb
-        # hardwire to simulate failure
-        flash[:warning] = "'#{params[:search_terms]}' was not found in TMDb."
-        redirect_to movies_path
+        @movies = Tmdb::Movie.find(params[:search_terms])
     end
     
     private
